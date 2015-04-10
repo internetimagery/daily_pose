ffed30c7bdfec1e39560c4d64efdbfe5a2ec72a4
Jason <jason.dixon.email@gmail.com>
2015-04-10 19:16:47 +1200

    building out data persistance

--- __init__.py
+++ __init__.py
@@ -1,12 +1,62 @@
 # Automate some daily pose tasks. Such as creating a folder and opening it up. All from maya
-import os, re, shutil
+import os, re, shutil, json, cPickle
 
 # Script directory
-base_path = os.path.dirname(os.path.realpath(__file__))
-template_path = os.path.join( base_path, "template" )
+base_path = os.path.dirname(os.path.realpath(__file__)) # Location of script
 
-# If template file doesn't exist. Create one.
-if not os.path.exists( template_path ):
-	os.makedirs( template_path )
-	print "Template file created. Add in files you wish to have in each pose. %s" % template_path
+# Template funtionality
+class Template(object):
+	def __init__(self):
+		self.path = os.path.join( base_path, "template" ) # Path for storing template file
+		if not os.path.exists( self.path ):
+			os.makedirs( self.path )
 
+class Data(object):
+	def __init__(self):
+		self.path = os.path.join( base_path, "data.json" ) # Path for storing data file
+		if not os.path.exists( self.path ):
+			open( self.path, "w").close()
+
+	def load(self):
+		try:
+			return json.load( self.path )
+		except e:
+			print "ERROR:", e
+			return {}
+
+	def save(self, data):
+		try:
+			f = open( self.path )
+			json.dump( data, f, encoding="utf-8", indent=4 )
+			f.close()
+			return True
+		except e:
+			print "Could not save data:", e
+			return False
+
+
+class DailyPose(object):
+	def __init__(self):
+		self.pose_dir = "" # directory for storing poses
+		self.loadData()
+		self.saveData()
+
+	# load stored data
+	def loadData(self):
+		try:
+			data = json.load( data_path )
+			self.pose_dir = data["pose_dir"]
+			print "success"
+		except:
+			pass
+
+	def saveData(self):
+		data = {"pose_dir":123}
+
+		f = open( data_path, "w" )
+		json.dump( ["data"], f, encoding="utf-8", indent=True )
+		f.close()
+
+		
+
+DailyPose()
